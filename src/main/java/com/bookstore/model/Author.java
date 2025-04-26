2aa822dd61d66cd8069a055b4937c99e5a4efa14
Venuvk0805 <rvenukanth5@gmail.com>
2025-04-26 19:36:30 +0530

    Modified Author.java

---
+++ src/main/java/com/bookstore/model/Author.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.model;
+
+/**
+ *
+ * @author USER
+ */
+public class Author {
+    private int id;         //unique id for storing authors 
+    private String name;
+    private String nationality;    //country of the author
+    
+    public Author(){};
+
+    public Author(int id, String name, String nationality) {
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "id=" + id + ", name=" + name + 
+                ", nationality=" + nationality + '}';
+    }
+    
+    
+    
+}
