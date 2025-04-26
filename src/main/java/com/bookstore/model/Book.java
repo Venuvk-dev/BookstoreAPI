47c04cefd692c39e05ad8f4db72c8b9df86b0ddc
Venuvk0805 <rvenukanth5@gmail.com>
2025-04-26 17:02:30 +0530

    Modified Book.java

--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -9,5 +9,55 @@ package com.bookstore.model;
  * @author USER
  */
 public class Book {
-    
+    private int id;                    //unique id for storing books
+    private String title;              //title name of the book
+    private String author;             //author's name
+    private double price;              
+
+    public Book(){};      //required for the api creating frameworks(jersey+jackson)
+
+    public Book(int id, String title, String author, double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", author="
+                + "" + author + ", price=" + price + '}';
+    }    //toString method may help in future to display the details of the instances
 }
