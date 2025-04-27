04fb3fe1ecde58d2f9ca7788b0f8c33d55c697c9
Venuvk0805 <rvenukanth5@gmail.com>
2025-04-27 18:58:53 +0530

    PUT update book method done

--- src/main/java/com/bookstore/resources/BookResource.java
+++ src/main/java/com/bookstore/resources/BookResource.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -72,6 +73,26 @@ public class BookResource {
         book.setId(nextId++);
         books.put(book.getId(), book);
         return Response.status(Response.Status.CREATED).entity(book).build();
+    }
+    
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateBook(@PathParam("id") int id, Book updatedBook){
+        logger.info("PUT request for updating the book of id : "+id);
+        Book existingBook=books.get(id);
+        if (existingBook == null) {
+            throw new BookNotFoundException("Book with the id "+id+" not found to update");
+        }
+        existingBook.setTitle(updatedBook.getTitle());
+        existingBook.setAuthor(updatedBook.getAuthor());
+        existingBook.setIsbn(updatedBook.getIsbn());
+        existingBook.setPublicationYear(updatedBook.getPublicationYear());
+        existingBook.setPrice(updatedBook.getPrice());
+        existingBook.setStockQuantity(updatedBook.getStockQuantity());
+        
+        books.put(id, existingBook);
+        return Response.ok(existingBook).build();
     }
 
     
