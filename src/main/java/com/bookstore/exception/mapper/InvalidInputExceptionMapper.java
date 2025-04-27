2e1fe5152cb28caef2c0fd4f027852c66e93390e
Venuvk0805 <rvenukanth5@gmail.com>
2025-04-27 12:12:12 +0530

    Created InvalidInputExceptionnMapper which will be mapped to exception class

---
+++ src/main/java/com/bookstore/exception/mapper/InvalidInputExceptionMapper.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.exception.mapper;
+
+import com.bookstore.exception.InvalidInputException;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author USER
+ */
+
+@Provider
+public class InvalidInputExceptionMapper implements ExceptionMapper<InvalidInputException>{
+    private static final Logger logger =LoggerFactory.getLogger(InvalidInputExceptionMapper.class);
+    
+    public Response toResponse(InvalidInputException exception){
+        logger.error("Invalid input entries : "+exception.getMessage());
+        
+        Map<String , String > errorMessage=new HashMap<>();
+        errorMessage.put("Error", exception.getMessage());
+        
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(errorMessage)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
