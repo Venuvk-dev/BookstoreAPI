0830b120e05b1f5a48b1947c0705340605736655
Venuvk0805 <rvenukanth5@gmail.com>
2025-04-27 12:17:26 +0530

    Created AuthorNotFoundExceptionMapper which will be mapped to exception class

---
+++ src/main/java/com/bookstore/exception/mapper/AuthorNotFoundExceptionMapper.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.exception.mapper;
+
+
+import com.bookstore.exception.AuthorNotFoundException;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author USER
+ */
+
+@Provider                                 //jax-rs will automatically detect this when server will started
+public class AuthorNotFoundExceptionMapper implements ExceptionMapper<AuthorNotFoundException>{
+    //log errors in the terminal for backend monitoring
+    private static final Logger logger=LoggerFactory.getLogger(AuthorNotFoundExceptionMapper.class);
+    
+    public Response toResponse(AuthorNotFoundException exception){
+        logger.error("Author not found : ", exception.getMessage());
+        
+        Map<String, String> errorMessage=new HashMap<>();       //a map to store 2 string values error and the particular error message
+        errorMessage.put("error", exception.getMessage());      
+        
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(errorMessage)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+    
+    
+}
